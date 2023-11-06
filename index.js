78ca5d61c3584099cea65b117b8c0b6a640b2653
Hack-it-Rafi <zozorafi2016@gmail.com>
2023-11-06 21:04:08 +0600

    jwt token generated

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5174'],
+    credentials: true
+}));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("MarketPlace server is running");
@@ -23,6 +29,13 @@ const client = new MongoClient(uri, {
     }
 });
 
+// my middleware
+const logger = async(req, res, next)=>{
+    console.log("IN LOGGER");
+    console.log('called: ', req.host, req.originalUrl);
+    next();
+}
+
 async function run() {
     try {
         // await client.connect();        
@@ -31,7 +44,7 @@ async function run() {
         const BidsCollection = database.collection("Bids");
 
         // JOBS
-        app.get("/categoryJobs", async(req, res) => {
+        app.get("/categoryJobs", async (req, res) => {
             const jobCat = req.query.category;
             // console.log(jobCat);
             const query = { category: jobCat };
@@ -42,9 +55,9 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get("/myJobs", async(req, res) => {
+        app.get("/myJobs", async (req, res) => {
             const myEmail = req.query.email;
-            const query = {employer_email: myEmail };
+            const query = { employer_email: myEmail };
             const options = {
                 sort: { job_title: 1 },
             };
@@ -53,59 +66,62 @@ async function run() {
             res.send(result);
         })
         // UPDATE myJobs
-        app.get("/myJobs/:id", async(req, res) => {
+        app.get("/myJobs/:id", async (req, res) => {
             const id = req.params.id;
             console.log(id);
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await JobsCollection.findOne(query);
             res.send(result);
         })
-        app.patch("/myJobs/:id", async(req,res)=>{
+        app.patch("/myJobs/:id", async (req, res) => {
             const id = req.params.id;
             const updatedJobs = req.body;
-            const filter = {_id: new ObjectId(id)};
-            const options = {upsert:true};
-            const product = {
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const job = {
                 //{name, image, brand, type, price, rating, description}
-                $set:{
-                    name: updatedJobs.name,
-                    image: updatedJobs.image,
-                    brand: updatedJobs.brand,
-                    type: updatedJobs.type,
-                    price: updatedJobs.price,
-                    rating: updatedJobs.rating,
+                $set: {
+                    // {email, title, deadline, category, minprice, maxprice, shortDes, longDes}
+                    employer_email: updatedJobs.email,
+                    job_title: updatedJobs.title,
+                    category: updatedJobs.category,
+                    deadline: updatedJobs.deadline,
+                    minimum_price: updatedJobs.minprice,
+                    maximum_price: updatedJobs.maxprice,
+                    short_description: updatedJobs.shortDes,
+                    long_description: updatedJobs.longDes,
                 }
             }
 
-            const result = await JobsCollectionCollection.updateOne(filter, product, options);
+            const result = await JobsCollection.updateOne(filter, job, options);
             res.send(result);
         })
 
         // DELETE 
-        app.delete('/myJobs/:id', async(req,res)=>{
+        app.delete('/myJobs/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await JobsCollection.deleteOne(query);
             res.send(result);
         })
-        
-        app.get('/jobs/:id', async(req,res)=>{
+
+        app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await JobsCollection.findOne(query);
             res.send(result);
         })
-        
 
-        app.post('/jobs', async(req,res)=>{
+
+        app.post('/jobs', async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
             const result = await JobsCollection.insertOne(newProduct);
             res.send(result);
         })
-        app.get('/jobs', async(req,res)=>{
+        app.get('/jobs', async (req, res) => {
             const cursor = JobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -113,18 +129,18 @@ async function run() {
 
         // BIDS
 
-        app.post('/bids', async(req, res)=>{
+        app.post('/bids', async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
             const result = await BidsCollection.insertOne(newProduct);
             res.send(result);
         })
-        app.get('/bids', async(req,res)=>{
+        app.get('/bids', async (req, res) => {
             const cursor = BidsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get("/myBids", async(req, res) => {
+        app.get("/myBids", async (req, res) => {
             const bidderMail = req.query.email;
             // console.log(jobCat);
             const query = { bidderEmail: bidderMail };
