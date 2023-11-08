38e8ba519ed745a9e27b5195d0b4ff08e1a29bd6
Hack-it-Rafi <zozorafi2016@gmail.com>
2023-11-08 17:20:09 +0600

    Almost completed

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+const corsOptions = {
+    origin: [
+        'http://localhost:5173',
+        'https://my-marketplace-3a996.web.app',
+        'https://my-marketplace-3a996.firebaseapp.com',
+    ],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
@@ -36,24 +42,24 @@ const logger = async (req, res, next) => {
     next();
 }
 
-const verifyToken = async(req,res,next)=>{
+const verifyToken = async (req, res, next) => {
     console.log("In VERIFYTOKEN");
-//     // console.log(req.cookies);
+    //     // console.log(req.cookies);
     const token = req.cookies?.token;
-    console.log("Value of token in middleware: ",token);
-    if(!token){
-        return res.status(401).send({message: "not authorized"})
+    console.log("Value of token in middleware: ", token);
+    if (!token) {
+        return res.status(401).send({ message: "not authorized" })
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if(err){
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
             console.log(err);
-            return res.status(401).send({message: 'not authorized'})
+            return res.status(401).send({ message: 'not authorized' })
         }
         console.log("Value in the token: ", decoded);
         req.user = decoded;
         next();
     })
-    
+
 }
 
 async function run() {
@@ -64,16 +70,23 @@ async function run() {
         const BidsCollection = database.collection("Bids");
 
         // JWT
-        app.post('/jwt', async(req, res)=>{
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
             console.log(user);
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res
-            .cookie('token', token, {
-                httpOnly: true,
-                secure: false,
-            })
-            .send({success: true})
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV==='production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                })
+                .send({ success: true })
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('Logging out: ', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
         })
 
         // JOBS
@@ -93,8 +106,8 @@ async function run() {
             const query = { employer_email: myEmail };
             console.log("Tok Token: ", req.cookies.token);
             console.log(myEmail, req.user.email);
-            if(myEmail!==req.user.email){
-                return res.status(403).send({message: "Forbidden Access"})
+            if (myEmail !== req.user.email) {
+                return res.status(403).send({ message: "Forbidden Access" })
             }
             const options = {
                 sort: { job_title: 1 },
@@ -180,17 +193,38 @@ async function run() {
         })
 
 
+        // app.get("/myBids", logger, verifyToken, async (req, res) => {
+        //     const bidderMail = req.query.email;
+        //     // console.log(jobCat);
+        //     const query = { bidderEmail: bidderMail };
+        //     const options = {
+        //         sort: { status: 1 },
+        //     };
+        //     const cursor = BidsCollection.find(query, options);
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // })
         app.get("/myBids", logger, verifyToken, async (req, res) => {
             const bidderMail = req.query.email;
-            // console.log(jobCat);
             const query = { bidderEmail: bidderMail };
-            const options = {
-                sort: { job_title: 1 },
+
+            const cursor = BidsCollection.find(query);
+            const results = await cursor.toArray();
+
+            // Define a custom sort order function
+            const customSort = (a, b) => {
+                const statusOrder = { pending: 1, "In Progress": 2, Rejected: 3, Complete: 4 };
+                return statusOrder[a.status] - statusOrder[b.status];
             };
-            const cursor = BidsCollection.find(query, options);
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+
+            // Sort the results using the custom sort order function
+            results.sort(customSort);
+
+            res.send(results);
+        });
+
+
+
         app.patch("/myBids/:id", logger, verifyToken, async (req, res) => {
             const id = req.params.id;
             const updatedStatus = req.body;
@@ -211,7 +245,7 @@ async function run() {
         app.get("/bidRequests", logger, verifyToken, async (req, res) => {
             const ownerEmail = req.query.email;
             // console.log(jobCat);
-            const query = {ownerEmail: ownerEmail };
+            const query = { ownerEmail: ownerEmail };
             const options = {
                 sort: { job_title: 1 },
             };
